4959f5a2cec26bdefd4cf43b1b3149239eba0257
Tugberk <34861636+tugberkcapraz@users.noreply.github.com>
2022-12-18 14:59:10 +0000

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Kerem Tugberk Capraz",
+  title: "Data Scientist",
+  email: "tugberkcapraz@gmail.com",
+  gitHub: "thttps://github.com/tugberkcapraz",
+  instagram: "@keremtugberk",
+  linkedIn: "https://www.linkedin.com/in/keremtugberk/",
+  medium: "https://medium.com/@keremtuberkapraz",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
